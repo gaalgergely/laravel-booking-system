0bbc919a0565d4ae770f5f09ae8160b764568b6e
Gergely, Gaal <gaalgergely@yahoo.com>
2020-09-08 20:20:38 +0200

    Centralized priceFor method

--- app/Http/Controllers/Api/BookablePriceController.php
+++ app/Http/Controllers/Api/BookablePriceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Bookable;
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BookablePriceController extends Controller
@@ -24,16 +23,8 @@ class BookablePriceController extends Controller
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
 
-        $days = (new Carbon($data['from']))->diffInDays(new Carbon($data['to'])) + 1;
-        $price = $days * $bookable->price;
-
         return response()->json([
-            'data' => [
-                'total' => $price,
-                'breakdown' => [
-                    $bookable->price => $days
-                ]
-            ]
+            'data' => $bookable->priceFor($data['from'], $data['to'])
         ]);
     }
 }
